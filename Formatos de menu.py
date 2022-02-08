717b2c07a3536b2dffed7e556a6cb9fe461fbeab
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-08 12:55:28 -0300

    Initial commit

---
+++ Formatos de menu.py
@@ -0,0 +1,11 @@
+def formato(s1):
+    print('|', '—' * 10,'|')
+    print('|', '—' * 10, '|')
+    print(s1)
+    print('|', '—' * 10, '|')
+    print('|', '—' * 10, '|')
+
+
+
+#Programa principal
+formato('  Alessandro')
