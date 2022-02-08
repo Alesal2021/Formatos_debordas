63a1f326c76ac20c31b5e475206a4faf33390ee1
Alesal2021 <alessandrocontato1975@gmail.com>
2022-02-08 13:40:53 -0300

    Initial commit

--- Rotinas de bordas.py
+++ Rotinas de bordas.py
@@ -12,9 +12,10 @@ def formato(s1):
 s1 = input('digite seu nome')
 print('{}{} '.format(s1,formato))"""
 ###################################################################
-def borda(s1):
-    tam = len(s1)
-    if tam:
+def borda(s1): # O s1 vai ser uma string
+    tam = len(s1)# Pego o tamanho da string com len
+    if tam: # Caso a  string form menor que 0 fica como False
+            # Se a string tem mais do que 0 retorna TRUE e impriome na tela
         print('|', '—' * tam, '|')
         print('|',s1,'|')
         print('|', '—' * tam, '|')
